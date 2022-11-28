d3973e5e9262afdfc59fd73588e23b3e4dae980e
Fjolnirr <yusufcan2617@gmail.com>
2022-11-28 14:51:54 +0300

    Add project files.

---
+++ Northwind.Business/Concrete/ProductManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.DataAccess.Concrete;
+using Northwind.Entities.Concrete;
+
+namespace Northwind.Business.Concrete
+{
+    public class ProductManager
+    {
+        ProductDal _productDal = new ProductDal();
+        public List<Product> GetAll()
+        {
+            //Business Code
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ Northwind.DataAccess/Concrete/NorthwindContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.Entities.Concrete;
+
+namespace Northwind.DataAccess.Concrete
+{
+    public class NorthwindContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
